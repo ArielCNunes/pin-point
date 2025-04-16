9dabd895cc1040557ae8ab5897c7a5b1da01ddf1
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-04-16 11:05:04 +0000

    Set Ionic mode to 'md' in provideIonicAngular

--- pinpoint-app/src/main.ts
+++ pinpoint-app/src/main.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideIonicAngular({ mode: 'md' }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
