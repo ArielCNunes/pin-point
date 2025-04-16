c2f714aacb5e7c1b84960c0178922825b6cbfc18
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-04-16 14:27:33 +0000

    Remove Firebase and environment configuration files for security reasons

--- pinpoint-app/src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC1RCBG-J9WClJjoagC_8gySgADkNom3oA",
-    authDomain: "pinpoint-app-58861.firebaseapp.com",
-    projectId: "pinpoint-app-58861",
-    storageBucket: "pinpoint-app-58861.firebasestorage.app",
-    messagingSenderId: "52970890558",
-    appId: "1:52970890558:web:4b7c4ead3eb523a21d386f"
-};
